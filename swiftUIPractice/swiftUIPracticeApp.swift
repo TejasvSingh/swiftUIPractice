bc3a8267d938acecc9d475ae7ce21cfa8b422892
TejasvSingh <singhtejasv98@gmail.com>
2025-10-01 14:54:17 -0500

    Initial Commit

---
+++ swiftUIPractice/swiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticeApp.swift
+//  swiftUIPractice
+//
+//  Created by Tejasv Singh on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
